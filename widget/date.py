c9f1a195cb267169391130151375de89e735a2de
typoverflow <typoverflow@outlook.com>
2022-08-05 21:47:27 +0800

    fix: lunar month and day

--- widget/date.py
+++ widget/date.py
@@ -6,7 +6,7 @@ from widget.base import BaseWidget
 from utils.log import log
 
 class DateWidget(BaseWidget):
-    YMC = [u"十一", u"十二", u"正", u"二", u"三", u"四", u"五", u"六", u"七", u"八", u"九", u"十" ]
+    YMC = [u"正", u"二", u"三", u"四", u"五", u"六", u"七", u"八", u"九", u"十", u"十一", u"十二" ]
     RMC = [u"初一", u"初二", u"初三", u"初四", u"初五", u"初六", u"初七", u"初八", u"初九", u"初十", u"十一", u"十二", u"十三", u"十四", u"十五", u"十六", u"十七", u"十八", u"十九", u"二十", u"廿一", u"廿二", u"廿三", u"廿四", u"廿五", u"廿六", u"廿七", u"廿八", u"廿九", u"三十", u"卅一"]
     GAN = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"]
     ZHI = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
@@ -50,8 +50,8 @@ class DateWidget(BaseWidget):
                 DateWidget.GAN[yTG.tg], 
                 DateWidget.ZHI[yTG.dz], 
                 "闰" if lunar_day.isLunarLeap() else "", 
-                DateWidget.YMC[lunar_day.getLunarMonth()], 
-                DateWidget.RMC[lunar_day.getLunarDay()], 
+                DateWidget.YMC[lunar_day.getLunarMonth()-1], 
+                DateWidget.RMC[lunar_day.getLunarDay()-1], 
                 DateWidget.JIEQI[lunar_day.getJieQi()] if lunar_day.hasJieQi() else "无节气"
             )
 
