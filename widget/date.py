93530697d80b1ad543c952593238084b7a764ada
typoverflow <typoverflow@outlook.com>
2022-08-05 21:24:05 +0800

    fix: sxtwl returns false lunar info

--- widget/date.py
+++ widget/date.py
@@ -10,14 +10,14 @@ class DateWidget(BaseWidget):
     RMC = [u"初一", u"初二", u"初三", u"初四", u"初五", u"初六", u"初七", u"初八", u"初九", u"初十", u"十一", u"十二", u"十三", u"十四", u"十五", u"十六", u"十七", u"十八", u"十九", u"二十", u"廿一", u"廿二", u"廿三", u"廿四", u"廿五", u"廿六", u"廿七", u"廿八", u"廿九", u"三十", u"卅一"]
     GAN = ["甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"]
     ZHI = ["子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"]
-    JIEQI =  ["无节气", "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑","白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪", "冬至"]
+    JIEQI =  ["冬至", "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑","白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪"]
     
     def __init__(self, config):
         super(DateWidget, self).__init__(config)
 
         self.date_info = None
         self.lunar_info = None
-        self.lunar = sxtwl.Lunar()
+        # self.lunar = sxtwl.Lunar()
 
         self.update_cycle = config.get("update_cycle", 1440)*60
         self.last_update = 0
@@ -43,14 +43,16 @@ class DateWidget(BaseWidget):
         try:
             self.old_lunar_info = self.lunar_info
             solar_day = time.localtime()
-            lunar_day = self.lunar.getDayBySolar(solar_day.tm_year, solar_day.tm_mon, solar_day.tm_mday)
+            lunar_day = sxtwl.fromSolar(solar_day.tm_year, solar_day.tm_mon, solar_day.tm_mday)
+            yTG = lunar_day.getYearGZ(True)
+            # lunar_day = self.lunar.getDayBySolar(solar_day.tm_year, solar_day.tm_mon, solar_day.tm_mday)
             self.lunar_info = "{}{}年 {}{}月{}  {}".format(
-                DateWidget.GAN[lunar_day.Lyear2.tg], 
-                DateWidget.ZHI[lunar_day.Lyear2.dz], 
-                "闰" if lunar_day.Lleap else "", 
-                DateWidget.YMC[lunar_day.Lmc], 
-                DateWidget.RMC[lunar_day.Ldi], 
-                DateWidget.JIEQI[lunar_day.jqmc]
+                DateWidget.GAN[yTG.tg], 
+                DateWidget.ZHI[yTG.dz], 
+                "闰" if lunar_day.isLunarLeap() else "", 
+                DateWidget.YMC[lunar_day.getLunarMonth()], 
+                DateWidget.RMC[lunar_day.getLunarDay()], 
+                DateWidget.JIEQI[lunar_day.getJieQi()] if lunar_day.hasJieQi() else "无节气"
             )
 
             log("green", "Request", "DateWidget - get lunar info successfully.")
